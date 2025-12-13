f959d2d016fdfd5b8c1eef50651e184d08efad2f
ds.kim <ollybaysion@gmail.com>
2025-12-13 21:30:24 +0900

    Initialize Spring Project

---
+++ src/test/java/com/dateguide/DateguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dateguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateguideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
