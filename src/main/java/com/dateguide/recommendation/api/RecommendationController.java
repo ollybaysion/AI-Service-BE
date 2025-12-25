303a26a190947ff8ee5ecbfac855f2003e47f720
ds.kim <ollybaysion@gmail.com>
2025-12-26 02:21:26 +0900

    /api/v1/recommendations API 명세 수정에 따른 변경 - jobId 를 반환하는 것으로 변경

--- src/main/java/com/dateguide/recommendation/api/RecommendationController.java
+++ src/main/java/com/dateguide/recommendation/api/RecommendationController.java
@@ -2,10 +2,13 @@ package com.dateguide.recommendation.api;
 
 import com.dateguide.recommendation.application.RecommendService;
 import com.dateguide.recommendation.dto.client.RecommendClientRequest;
-import com.dateguide.recommendation.dto.client.RecommendClientResponse;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Client 로부터 요청을 받아 LLM 으로 전달 후 jobId 를 반환
+ * LLM 으로부터 결과를 받으면 SSE 로 Client 에게 전달
+ */
 @RestController
 @RequestMapping("/api/v1/recommendations")
 public class RecommendationController {
@@ -19,7 +22,7 @@ public class RecommendationController {
     }
 
     @PostMapping
-    public RecommendClientResponse createRecommendation(@RequestBody RecommendClientRequest recommendClientRequest) {
-        return recommendService.recommend(recommendClientRequest);
+    public String requestRecommendation(@RequestBody RecommendClientRequest recommendClientRequest) {
+        return recommendService.recommendAsync(recommendClientRequest);
     }
 }
